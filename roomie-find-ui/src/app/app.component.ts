0da3014ebfe7b062732e416fbdd0b2cd959a8905
mattygersh21 <dakotaseia@hotmail.com>
2024-06-15 23:27:53 -0500

    * updated app.component.html * added login component * added register component * added routing to new components

--- roomie-find-ui/src/app/app.component.ts
+++ roomie-find-ui/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'roomie-find-ui';
+  http = inject(HttpClient);
+
+  logout(): void {
+    console.log('logout');
+  }
 }
---
+++ roomie-find-ui/src/app/components/login/login.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+})
+export class LogicComponent {
+  fb = inject(FormBuilder);
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  form = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  errorMessage: string | null = null;
+
+  onSubmit(): void {
+    console.log('login');
+  }
+}
